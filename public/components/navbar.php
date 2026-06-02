36a2710c4c4993c4300141600c20f495c4939250
Pedro <pedro_vieira-juni@estudante.sesisenai.org.br>
2026-06-02 09:07:34 -0300

    add - comentarios feitos na NAVBAR
    -pasta components
    -chamado de outra página

---
+++ public/components/navbar.php
@@ -0,0 +1,10 @@
+
+
+<header>
+    <nav>
+        <h2> Sistema Simples </h2>
+    </nav>
+</header>
+
+<!-- componete de navbar que esta sendo chamado de outra página -->
+ <!-- esta na pasta COMPONENTS -->
