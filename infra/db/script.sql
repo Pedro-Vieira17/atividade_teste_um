f19583b0dd31eaa25bf574ad0a44668893cf406c
Pedro <pedro_vieira-juni@estudante.sesisenai.org.br>
2026-06-02 09:27:25 -0300

    add - comentarios do script.sql
    -database
    -usar e criar tabela
    -inserção de usuario e senha

--- infra/db/script.sql
+++ infra/db/script.sql
@@ -1,11 +1,15 @@
-CREATE DATABASE sistema_simples;
+-- criação da database
+CREATE DATABASE sistema_simples; 
 
+-- usar tabela
 USE sistema_simples;
 
+-- criação tabela user(id, username, password)
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
+-- inserção de um username e uma password, tais como: "admin" e "123" respectivamente
 INSERT INTO users (username, password) VALUES ('admin', '123');
