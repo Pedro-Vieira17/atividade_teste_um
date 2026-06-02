d5b31eae465effb84b30f2fa20a0fa03fd723180
Pedro <pedro_vieira-juni@estudante.sesisenai.org.br>
2026-06-02 09:31:23 -0300

    add - comentarios do connect.php
    -armazenação
    -conexão
    -mostrar se ouve conexão

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,12 +1,15 @@
 <?php
 
+    // armazena os dados de acesso ao banco de dados
     $host = "localhost";
     $user = "root";
     $pass = "root";
     $db = "sistema_simples";
 
+    // abre a conexão entre o PHP e o banco MySQL
     $conn = new mysqli($host,$user,$pass,$db);
 
+    // mostrar se ouve a conexão com o banco ou não
     if ($conn->connect_error){
         echo "<script> console.log('erro na conexão com o banco') </script>";
     }else{
